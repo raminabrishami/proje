285967cabd39a42090774dc18758de826e365386
raminabrishami <125660586+raminabrishami@users.noreply.github.com>
2023-04-19 13:57:46 +0330

    Update prige.py

--- prige.py
+++ prige.py
@@ -2,40 +2,55 @@ import os
 from dash import Dash,dcc,Output,Input
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+import plotly.graph_objects as go
 
-app=Dash(__name__,external_stylesheets={dbc.themes.BOOTSTRAP})
-div=html.Div()
+app=Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-btn=dbc.Button('جستو جو')
+text=html.H3('',style={'margin-top':'40px'})
+
+inputt=dbc.Input(value='taip',style={'margin-top':'40px'})
+
+btn=dbc.Button('جستو جو',style={'margin-top':'40px'})
+
+div=html.Div(style={'margin-top':'60px'})
+
+nemodar=dcc.Graph(figure={})
 
 @app.callback(
-
-    Output(div,component_property='figure'),
+    Output(text,component_property='children'), 
+    Output(div,component_property='children'),
     Output(btn,component_property='n_clicks'),
+    Output(nemodar, component_property='figure'),
+    Input(inputt,component_property='value'),
     Input(btn,component_property='n_clicks')
     )
 
-def create_table(n_clicks):
+
+
+def update_text(inp_text,n_clicks):
+    global ll
     if n_clicks>0:
-        row=[html.Th('تصویر'),html.Th('قیمت'), html.Th('محصول')]
-        with open ('title' , encoding='utf-8') as reads:
-            title=reads.read().split('/n')
-        with open ('price') as reads:
-            price=reads.read().split('/n')
-        with open ('links') as reads:
-            links=reads.read().split('/n')
-        image_list=os.listdir('asset/images')
+        row=[html.Tr([html.Th('تصویر'),html.Th('قیمت'), html.Th('محصول')])]
+        with open ('title.txt' , encoding='utf-8') as reads:
+            title=reads.read().split('\n')
+        with open ('price.txt') as reads:
+            price=reads.read().split('\n')
+        with open ('links.txt') as reads:
+            links=reads.read().split('\n')
+        image_list=os.listdir('assets/images')
         for t,p,l,image in zip(title,price,links,image_list):
             img=html.Img(src=app.get_asset_url(f'images/{image}'),width='100px' ,height='100px')
-            v=html.Tr(html.Td(img),html.Td(p),html.A(t,href=l))
+            v=html.Tr([html.Td(img),html.Td(p),html.A(t,href=l)] )
             row.append(v)
-        child=[html.Table(row,style={'wedth':'100px'})]
-        return child , n_clicks
- 
-app.layout=dbc.Container([
+        children=[html.Table(row,style={'width':'100%'})]
+        price=list(map(int,price))
+        fig = go.Figure(data=[go.Scatter(x=list(range(0,len(price))), y=price)])
+        ll=1
+        return inp_text,children,n_clicks,fig
 
-dbc.Row([div,btn],class_name='text-center')
-])
+ 
+app.layout=dbc.Container([dbc.Row([inputt,btn,text,div,nemodar],className='text-center')])
+
 
 if __name__=='__main__':
-    app.run_server(port='8000')
+    app.run_server(debug=True,port='8000')
